4fb4e94bd3fcdc7f3012d39e2347c233f12812df
Murali Suriar <code@suriar.com>
2014-10-09 02:20:51 +0100

    Basic behaviour test to move update logic into UsefulItem.

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -3,8 +3,18 @@ require "rspec"
 
 describe GildedRose do
 
-  it "should do something" do
+  it "should do something after one update" do
+    expected = []
+    expected << UsefulItem.new("+5 Dexterity Vest", 9, 19)
+    expected << UsefulItem.new("Aged Brie", 1, 1)
+    expected << UsefulItem.new("Elixir of the Mongoose", 4, 6)
+    expected << UsefulItem.new("Sulfuras, Hand of Ragnaros", 0, 80)
+    expected << UsefulItem.new("Backstage passes to a TAFKAL80ETC concert", 14, 21)
+    expected << UsefulItem.new("Conjured Mana Cake", 2, 5)
+
     subject.update_quality
+
+    subject.items.should eq expected
   end
 
   it "should be able to compare items" do
