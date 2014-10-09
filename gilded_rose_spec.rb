b85eb71138e74ad0b2609887845dc8a4b2019344
Murali Suriar <code@suriar.com>
2014-10-09 02:08:03 +0100

    Add equality test.

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -7,5 +7,10 @@ describe GildedRose do
     subject.update_quality
   end
 
+  it "should be able to compare items" do
+    foo = UsefulItem.new("+5 Dexterity Vest", 10, 20)
+    bar = UsefulItem.new("+5 Dexterity Vest", 10, 20)
 
+    foo.should eq bar
+  end
 end
