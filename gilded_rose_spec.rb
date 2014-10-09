083067afc8a574cd2775c475cc680b9e9cd697ae
Murali Suriar <code@suriar.com>
2014-10-09 11:59:55 +0100

    Remove unnecessary newlines.

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -11,30 +11,25 @@ describe GildedRose do
     expected << UsefulItem.new("Sulfuras, Hand of Ragnaros", 0, 80)
     expected << UsefulItem.new("Backstage passes to a TAFKAL80ETC concert", 14, 21)
     expected << UsefulItem.new("Conjured Mana Cake", 2, 5)
-
     subject.update_quality
-
     subject.items.should eq expected
   end
 
   it "should be able to compare items" do
     foo = UsefulItem.new("+5 Dexterity Vest", 10, 20)
     bar = UsefulItem.new("+5 Dexterity Vest", 10, 20)
-
     foo.should eq bar
   end
 
   it "should be able to compare lists" do
     foo = [UsefulItem.new("+5 Dexterity Vest", 10, 20)]
     bar = [UsefulItem.new("+5 Dexterity Vest", 10, 20)]
-
     foo.should eq bar
   end
 
   it "should be checking item list contents" do
     foo = [UsefulItem.new("+5 Dexterity Vest", 10, 39)]
     bar = [UsefulItem.new("+5 Dexterity Vest", 10, 20)]
-
     foo.should_not eq bar
   end
 end
