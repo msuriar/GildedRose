a51938db815e0622776ade93fd6514292cd23c71
Murali Suriar <code@suriar.com>
2014-10-09 02:25:51 +0100

    Move update code into UsefulItem.

--- item.rb
+++ item.rb
@@ -1,7 +1,7 @@
 class Item
 
   attr_accessor :name, :sell_in, :quality
-        
+
   def initialize (name, sell_in, quality)
     @name = name
     @sell_in = sell_in
@@ -17,5 +17,51 @@ class UsefulItem < Item
     self.name == another_item.name\
     && self.sell_in == another_item.sell_in\
     && self.quality == another_item.quality
+  end
+
+  def update
+      if (self.name != "Aged Brie" && self.name != "Backstage passes to a TAFKAL80ETC concert")
+        if (self.quality > 0)
+          if (self.name != "Sulfuras, Hand of Ragnaros")
+            self.quality = self.quality - 1
+          end
+        end
+      else
+        if (self.quality < 50)
+          self.quality = self.quality + 1
+          if (self.name == "Backstage passes to a TAFKAL80ETC concert")
+            if (self.sell_in < 11)
+              if (self.quality < 50)
+                self.quality = self.quality + 1
+              end
+            end
+            if (self.sell_in < 6)
+              if (self.quality < 50)
+                self.quality = self.quality + 1
+              end
+            end
+          end
+        end
+      end
+      if (self.name != "Sulfuras, Hand of Ragnaros")
+        self.sell_in = self.sell_in - 1;
+      end
+      if (self.sell_in < 0)
+        if (self.name != "Aged Brie")
+          if (self.name != "Backstage passes to a TAFKAL80ETC concert")
+            if (self.quality > 0)
+              if (self.name != "Sulfuras, Hand of Ragnaros")
+                self.quality = self.quality - 1
+              end
+            end
+          else
+            self.quality = self.quality - self.quality
+          end
+        else
+          if (self.quality < 50)
+            self.quality = self.quality + 1
+          end
+        end
+      end
   end
 end
