d2421f52afb5843b6f2d92a78c097dfe9a5e63d2
Murali Suriar <code@suriar.com>
2014-10-09 11:21:51 +0100

    Fix indentation.

--- item.rb
+++ item.rb
@@ -20,48 +20,48 @@ class UsefulItem < Item
   end
 
   def update
-      if (self.name != "Aged Brie" && self.name != "Backstage passes to a TAFKAL80ETC concert")
-        if (self.quality > 0)
-          if (self.name != "Sulfuras, Hand of Ragnaros")
-            self.quality = self.quality - 1
+    if (self.name != "Aged Brie" && self.name != "Backstage passes to a TAFKAL80ETC concert")
+      if (self.quality > 0)
+        if (self.name != "Sulfuras, Hand of Ragnaros")
+          self.quality = self.quality - 1
+        end
+      end
+    else
+      if (self.quality < 50)
+        self.quality = self.quality + 1
+        if (self.name == "Backstage passes to a TAFKAL80ETC concert")
+          if (self.sell_in < 11)
+            if (self.quality < 50)
+              self.quality = self.quality + 1
+            end
           end
+          if (self.sell_in < 6)
+            if (self.quality < 50)
+              self.quality = self.quality + 1
+            end
+          end
+        end
+      end
+    end
+    if (self.name != "Sulfuras, Hand of Ragnaros")
+      self.sell_in = self.sell_in - 1;
+    end
+    if (self.sell_in < 0)
+      if (self.name != "Aged Brie")
+        if (self.name != "Backstage passes to a TAFKAL80ETC concert")
+          if (self.quality > 0)
+            if (self.name != "Sulfuras, Hand of Ragnaros")
+              self.quality = self.quality - 1
+            end
+          end
+        else
+          self.quality = self.quality - self.quality
         end
       else
         if (self.quality < 50)
           self.quality = self.quality + 1
-          if (self.name == "Backstage passes to a TAFKAL80ETC concert")
-            if (self.sell_in < 11)
-              if (self.quality < 50)
-                self.quality = self.quality + 1
-              end
-            end
-            if (self.sell_in < 6)
-              if (self.quality < 50)
-                self.quality = self.quality + 1
-              end
-            end
-          end
         end
       end
-      if (self.name != "Sulfuras, Hand of Ragnaros")
-        self.sell_in = self.sell_in - 1;
-      end
-      if (self.sell_in < 0)
-        if (self.name != "Aged Brie")
-          if (self.name != "Backstage passes to a TAFKAL80ETC concert")
-            if (self.quality > 0)
-              if (self.name != "Sulfuras, Hand of Ragnaros")
-                self.quality = self.quality - 1
-              end
-            end
-          else
-            self.quality = self.quality - self.quality
-          end
-        else
-          if (self.quality < 50)
-            self.quality = self.quality + 1
-          end
-        end
-      end
+    end
   end
 end
