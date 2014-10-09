b40c2a06292cb904089ddfdd7bf61abe5fb43502
Murali Suriar <code@suriar.com>
2014-10-09 02:12:04 +0100

    Verify that comparison of UsefulItem lists works.

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -13,4 +13,18 @@ describe GildedRose do
 
     foo.should eq bar
   end
+
+  it "should be able to compare lists" do
+    foo = [UsefulItem.new("+5 Dexterity Vest", 10, 20)]
+    bar = [UsefulItem.new("+5 Dexterity Vest", 10, 20)]
+
+    foo.should eq bar
+  end
+
+  it "should be checking item list contents" do
+    foo = [UsefulItem.new("+5 Dexterity Vest", 10, 39)]
+    bar = [UsefulItem.new("+5 Dexterity Vest", 10, 20)]
+
+    foo.should_not eq bar
+  end
 end
